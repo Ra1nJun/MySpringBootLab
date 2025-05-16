88d765900c96c06b4d28326f61fb628a9fac4288
Ra1nJun <Over4ction@gmail.com>
2025-05-16 16:14:57 +0900

    cors config add

--- src/main/java/com/rookies3/myspringbootlab/service/BookService.java
+++ src/main/java/com/rookies3/myspringbootlab/service/BookService.java
@@ -9,12 +9,10 @@ import com.rookies3.myspringbootlab.repository.BookDetailRepository;
 import com.rookies3.myspringbootlab.repository.BookRepository;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
